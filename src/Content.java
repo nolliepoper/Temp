c93e25cb412c8520d5cb8eee03480026138bab4b
Branden Kroske <b.kroske@gmail.com>
2013-11-22 17:01:58 -0500

    Fixed the spawn points.

--- src/Content.java
+++ src/Content.java
@@ -174,44 +174,37 @@ public class Content extends JPanel
         {
             //Read the data about the room
             currRoom.setRoomName(currRoom.getNorth());
+			currSpawn = currRoom.getSouthSpawn();
             reloadRoom();
             Map.defaultMap.Update(0, -1, currRoom);
-            currSpawn = currRoom.getSouthSpawn();
-            //isMove = true;
         }
         //Go south?
         else if(currPos.y > frame.getHeight())
         {
             //Read the data about the room
             currRoom.setRoomName(currRoom.getSouth());
+            currSpawn = currRoom.getNorthSpawn();
             reloadRoom();
             Map.defaultMap.Update(0, 1, currRoom);
-            currSpawn = currRoom.getNorthSpawn();
-            //isMove = true;
         }
         //Go east?
         else if(currPos.x > frame.getWidth())
         {
             //Read the data about the room
             currRoom.setRoomName(currRoom.getEast());
+            currSpawn = currRoom.getWestSpawn();
             reloadRoom();
 			Map.defaultMap.Update(1, 0, currRoom);
-            currSpawn = currRoom.getWestSpawn();
-            //isMove = true;
         }
         //Go west?
         else if(currPos.x < 0)
         {
             //Read the data about the room
             currRoom.setRoomName(currRoom.getWest());
+            currSpawn = currRoom.getEastSpawn();
             reloadRoom();
 			Map.defaultMap.Update(-1, 0, currRoom);
-            currSpawn = currRoom.getEastSpawn();
-            //isMove = true;
         }
-
-        //If the player is actually moving to a new room, recreate 
-        //the managers
 	}
 	
     public void setBloodOverlay()
