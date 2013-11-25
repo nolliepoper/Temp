41e6fca6361601d5176ee5c6bbdecee0839c84ef
Gonkalbell <johnmichaelbell@gmail.com>
2013-11-25 14:04:09 -0500

    Fixed a glitch with easy mode and the map

--- src/Enemy.java
+++ src/Enemy.java
@@ -32,18 +32,17 @@ public abstract class Enemy extends Entity
 	// Constructor.
 	Enemy()
 	{
-		super(new Vector(50, 50), 50, 50);
+		this(new Vector(50, 50), 50, 50);
 		alive = true;
-		if(Frame.easyMode)
-			health = 3;
-		else
-			health = 5;
 	}
 	public Enemy(Vector vIn, int wIn, int hIn)
 	{
 		super(vIn, wIn, hIn);
 		alive = true;
-		health = 5;
+		if(Frame.easyMode)
+			health = 3;
+		else
+			health = 5;
 	}
 	public void setHp(int hp)
 	{
